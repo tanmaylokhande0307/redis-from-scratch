bb7eece80550da1412bfc4b6db29e627a8f605c8
debian <tanmay.codes0307@gmail.com>
2025-03-19 01:59:40 +0530

    resp

--- main.go
+++ main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"fmt"
-	"io"
 	"net"
-	"os"
 )
 
 func main() {
@@ -32,16 +30,17 @@ func main() {
 	//create infinite loop to receive commands from the client and execute them
 
 	for {
-		buffer := make([]byte, 1024)
+		
+		resp := NewResp(conn)
 
-		_, err = conn.Read(buffer)
+		value, err := resp.Read()
+		
 		if err != nil {
-			if err == io.EOF {
-				break
-			}
-			fmt.Println("error reading from client: ", err.Error())
-			os.Exit(1)
+			fmt.Println(err)
+			return
 		}
+
+		fmt.Println(value)
 
 		conn.Write([]byte("+OK\r\n")) //according to RESP + = simple string
 
